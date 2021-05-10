8bf562a1ef7d5e072dde00ed2ae6883c079713ae
ms471841 <ms471841@.com>
2021-05-10 11:36:59 +0530

    python variables

---
+++ varibles.py
@@ -0,0 +1,39 @@
+#variables in python
+
+
+
+#we can declare a variable by assign a value to it...
+x=10   #x is a varibles which holds value 10  and is type is int
+print(x)  #value of x will be print
+
+x='10' #value of x will be overwrite by '10' and type is str
+print(x)
+
+x=10.0 #value of x will be overwrite by 10.0 and type is double
+print(x)
+
+#we can give explicit the data type to a varible by casting
+
+x=str (10)  #value of x is str type 
+print(x)
+
+x=int(20.0)   #value of x will be int type
+print(x)
+
+x=float (30)#value of x will be float type
+print (x)
+
+#we also get the datatype of any varible by using type()
+
+x=10
+print(type(x))
+x='manish'
+print(type(x))
+
+#we also get for others 
+
+#tata see you in the next program
+
+
+
+
