203e6065b3b09be2e4c8eca781d67c594270dbae
ms471841 <ms471841@.com>
2021-05-10 10:39:12 +0530

    multi line comments

--- comments.py
+++ comments.py
@@ -4,3 +4,11 @@
 print("In python comments are made with hash #") #this is the print statement
 
 
+#Multi line comments are made by insert # in each line 
+#this is comment
+#making multiline comments
+#or you may also use quotes to make a multiline comments
+
+'''this is the multiline comment
+i am try to make multiline comment
+ohh great ....multiline comment is created'''
