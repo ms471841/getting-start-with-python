9d306ddfdf1b835dbe66f8d2823285116e852898
ms471841 <ms471841@.com>
2021-05-11 11:30:22 +0530

    data types in python

---
+++ dataTypes.py
@@ -0,0 +1,38 @@
+#today we gonna start data types
+
+from ast import increment_lineno
+
+
+x=int(4)
+y=float(5)
+z=str("manish")
+xy=complex(4j)
+print(x,y,z,xy)
+print(type(xy))
+
+
+x=list([2,3,5,6,])
+y=tuple((4,5,87,4,43,7))
+z=range(6)
+xy=dict(name="manish",age=26)
+print(x,y,z,xy)
+
+x=set(("manish","noopur","waris","mrityunjay","satendra"))
+y=frozenset(("manish","waris"))
+z=bool(5)
+xy=bytes(5)
+print(x,y,z,xy)
+
+
+#type conversion
+
+
+x=int(4.6)
+y=float(4)
+z=str(4)
+print(x,y,z)
+
+
+#tata .....see in next program😀
+
+
