3f7a79dfd2c4d4d9f9b87d36d271073ab073d9e7
ms471841 <ms471841@.com>
2021-05-12 07:59:42 +0530

    random numbers

---
+++ randomNumbers.py
@@ -0,0 +1,10 @@
+#Random  numbers in python
+
+
+import random
+#randrange function takes three parameters start,stop, width
+
+print(random.randrange(1,20))  #without width
+
+
+print(random.randrange(20,40,5))  #with width
